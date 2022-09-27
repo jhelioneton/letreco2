3c7a367ec99934b84e15d047907fde7a805ddbf5
Helio <jhelioneto@hotmail.com>
2022-09-27 20:22:23 -0300

    Fim do dia

---
+++ index.js
@@ -0,0 +1,17 @@
+const letras = document.querySelector(".letras");
+
+const tentativas = 6;
+const colunas = 5;
+
+for (let tentativaIndex = 0; tentativaIndex < tentativas; tentativaIndex++) {
+    const tentativaLetra = document.createElement("div");
+    tentativaLetra.setAttribute("id", tentativaIndex);
+    tentativaLetra.setAttribute("class", tentativa - letra);
+   
+    for (let colunaIndex = 0; colunaIndex < array.length; colunaIndex++) {
+        const colunaLetra = document
+        
+    }
+}
+
+//AMANHÃ ALTERAR OS INDEX DE PT PARA EN PRA NÃO SE FICAR CONFUSO!!!!!!!!!
