78c2536cb1ef135dccf0e4f392ee5b4aebc9cd88
Helio <jhelioneto@hotmail.com>
2022-09-28 12:41:40 -0300

    Fileiras e colunas criadas

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const rows = 6; //Numero de tentativas.
 const columns = 5; //Numero de letras.
 
 // Criar as fileiras e colunas.
-for (let rowIndex = 0; rowIndex < rows; rowsIndex++) {
+for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
     const tileRow = document.createElement("div");
     tileRow.setAttribute("id", "row" + rowIndex); // Dar id para a fileira.
     tileRow.setAttribute("class", "tile-row"); // Dar uma classe para a fileira.
 
     for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
         const tileColumn = document.createElement("div");
-        tileColumn.setAttribute("row" + rowIndex + "columns" + columnIndex); // Dar id da coluna separada da fileira.
-        tileColumn.setAttribute("tile-column"); // Dar classe para coluna.
+        tileColumn.setAttribute("id", "row" + rowIndex + "columns" + columnIndex); // Dar id da coluna separada da fileira.
+        tileColumn.setAttribute("class", "tile-column"); // Dar classe para coluna.
         tileRow.append(tileColumn); // Adicionar a coluna na fileira.
     }
   letras.append(tileRow); // Adicionar a fileira.
