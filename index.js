64c183bcb864b448274141c4ea55bb0c859b718c
Helio <jhelioneto@hotmail.com>
2022-09-28 13:10:49 -0300

    Criada as consts das teclas

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const letras = document.querySelector(".letras");
-const apagareEnter = document.querySelector ("#apagareEnter"); // Botão de apagar e enter.
-const primeiraLinhaTeclado = document.querySelector ("#primeiraLinhaTeclado"); // Primeira linha do teclado.
-const segundaLinhaTeclado = document.querySelector ("#segundaLinhaTeclado"); // Segunda linha do teclado.
-const terceiraLinhaTeclado = document.querySelector ("#terceiraLinhaTeclado"); // Terceira linha do Teclado.
+const apagareEnter = document.querySelector("#apagareEnter"); // Botão de apagar e enter.
+const primeiraLinhaTeclado = document.querySelector("#primeiraLinhaTeclado"); // Primeira linha do teclado.
+const segundaLinhaTeclado = document.querySelector("#segundaLinhaTeclado"); // Segunda linha do teclado.
+const terceiraLinhaTeclado = document.querySelector("#terceiraLinhaTeclado"); // Terceira linha do Teclado.
+
+const teclasPrimeiraLinha = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+const teclasSegundaLinha = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+const teclasTerceiraLinha= ["Z", "X", "C", "V", "B", "N", "M"];
 
 const rows = 6; //Numero de tentativas.
 const columns = 5; //Numero de letras.
@@ -19,5 +23,5 @@ for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
         tileColumn.setAttribute("class", "tile-column"); // Dar classe para coluna.
         tileRow.append(tileColumn); // Adicionar a coluna na fileira.
     }
-  letras.append(tileRow); // Adicionar a fileira.
+    letras.append(tileRow); // Adicionar a fileira.
 }
