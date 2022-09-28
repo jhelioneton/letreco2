999742363bbad3732b2334531a4c5f7393749fc9
Helio <jhelioneto@hotmail.com>
2022-09-28 12:23:50 -0300

    Alterações em relação a tradução feitas

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const letras = document.querySelector(".letras"); 
 
-const tentativas = 6;
-const colunas = 5;
+const rows = 6; //Numero de tentativas.
+const columns = 5; //Numero de letras.
 
-for (let tentativaIndex = 0; tentativaIndex < tentativas; tentativaIndex++) {
-    const fileiraTentativa = document.createElement("div");
-    tentativaLetra.setAttribute("id", "tentativa" + tentativaIndex); //dar id
-    tentativaLetra.setAttribute("class", "fileira-tentativa"); //dar uma classe
+// Criar as fileiras e colunas.
+for (let rowIndex = 0; rowIndex < rows; rowsIndex++) {
+    const tileRow = document.createElement("div");
+    tileRow.setAttribute("id", "row" + rowIndex); // Dar id para a fileira.
+    tileRow.setAttribute("class", "tile-row"); // Dar uma classe para a fileira.
 
-    for (let colunaIndex = 0; colunaIndex < colunas; colunaIndex++) {
-        const espacoLetra = document.createElement("div");
-        espacoLetra.setAttribute("tentativa" + tentativaIndex + "colunas" + colunaIndex);
-        espacoLetra.setAttribute("espaco-letra");
-        tentativaLetra.append(espacoLetra);
+    for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
+        const tileColumn = document.createElement("div");
+        tileColumn.setAttribute("row" + rowIndex + "columns" + columnIndex); // Dar id da coluna separada da fileira.
+        tileColumn.setAttribute("tile-column"); // Dar classe para coluna.
+        tileRow.append(tileColumn); // Adicionar a coluna na fileira.
     }
+  letras.append(tileRow); // Adicionar a fileira.
 }
